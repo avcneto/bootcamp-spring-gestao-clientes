d7b25b6e58a14c0fa79ef5a746e104bf157efd61
Aderson <aderson.neto@mercadolivre.com>
2021-12-28 12:30:09 -0300

    first commit

---
+++ src/main/java/br/com/gestao/bootcampspringgestaoclientes/BootcampSpringGestaoClientesApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestao.bootcampspringgestaoclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampSpringGestaoClientesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootcampSpringGestaoClientesApplication.class, args);
+    }
+
+}
